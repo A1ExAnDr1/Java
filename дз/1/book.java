1d75dc9f44fbd7fed916436ee6f2d16eaf12fdf0
Alexandr <93984557+A1ExAnDr1@users.noreply.github.com>
2021-11-13 17:54:00 +0300

    Update book.java

--- дз/1/book.java
+++ дз/1/book.java
@@ -1,1 +1,38 @@
+пакет  ru.mirea.pr1 ;
 
+public  class  Book {
+    частное  строковое имя;
+    частные  int страницы;
+
+    общедоступная  книга ( String  n , int  a ) {
+        name = n;
+        pages = a;
+    }
+    public  Book ( String  n ) {
+        name = n;
+        страницы =  0 ;
+    }
+    public  Book () {
+        name =  " 42 " ;
+        pages =  42 ;
+    }
+
+    public  void  setPages ( int  age ) {
+        это . страницы = возраст;
+    }
+    public  void  setName ( String  Name ) {
+        это . name = name;
+    }
+    public  int  getPages () {
+        вернуть страницы;
+    }
+    public  String  getName () {
+        возвращаемое имя;
+    }
+    public  String  toString () {
+        верни  это . name + " включает " + это . страницы + " страницы. " ;
+    }
+    public  void  howLongToRead () {
+        Система . из . println ( " Требуется " + (pages / 2 ) / 60 + " часов и " + (pages / 2 ) % 60 + " минут для чтения " + имя);
+    }
+}
